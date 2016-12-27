5dd1cb94e8d150ec16b280620b9acdf4da475510
pattysc <pattys@users.noreply.github.com>
2016-12-26 19:30:05 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
 
 end
